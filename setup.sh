7238182f6189808c3a3f0a8cddb601b4849da8ce
Martin Eden <martin.s.eden@pm.me>
2018-09-10 14:01:55 +0100

    Improve detection of existing installation

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-file_name=duplicati_2.0.3.3-1_all.deb
+sudo apt-get -q install gdebi cron python3-pip -y
 
-sudo apt-get -q install gdebi cron -y
-if [ ! -f ${file_name} ]; then
+if which -a duplicati-cli > /dev/null; then
+    echo "Duplicati is already installed"
+else
+    echo "installing Duplicati"
+    file_name=duplicati_2.0.3.3-1_all.deb
     wget https://updates.duplicati.com/beta/${file_name}
+    sudo gdebi --non-interactive ${file_name}
 fi
-dpkg -s duplicati | grep -q "Status: install ok installed" \
-    || sudo gdebi --non-interactive ${file_name}
 
-sudo apt-get install python3-pip -y
 sudo pip3 install --quiet -r ${BASH_SOURCE%/*}/requirements.txt
 
 export VAULT_ADDR='https://support.montagu.dide.ic.ac.uk:8200'
